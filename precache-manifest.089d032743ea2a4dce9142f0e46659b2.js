8ab21bebfaa827e506984842f63da2b56090c15e
laouali baraou <youssoufoulaoualibaraou@gmail.com>
2020-10-09 19:35:51 +0100

    Updates

---
+++ precache-manifest.089d032743ea2a4dce9142f0e46659b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465803f1fa8e739708d84dd85aeca266",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "dbaf4bee17f6b80ef347",
+    "url": "/youtube/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1be7d9b71131dfcc4263",
+    "url": "/youtube/static/js/2.813b96f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube/static/js/2.813b96f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbaf4bee17f6b80ef347",
+    "url": "/youtube/static/js/main.530861c6.chunk.js"
+  },
+  {
+    "revision": "66d15f02628b16631539",
+    "url": "/youtube/static/js/runtime-main.fd6ec26e.js"
+  }
+]);
